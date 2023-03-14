dfe2256b810ec6f02e121b94b65bde78f943a951
sehyun <germs1020@gmail.com>
2023-03-15 01:05:39 +0900

    네브바, 검색, 글목록 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useParams } from 'react-router-dom';
+import { Routes, Route, useNavigate, Outlet} from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <div className='header'>
-        <div className='navbar'>
-          <h1>Fire Sea</h1>
-          <a>server</a>
-          <a>front</a>
-          <button onClick={()=>{
+
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<Input/>}/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+
+      <div className='content'>
+
+        <div className='board'>
+          <h1 className='board-category'>server</h1>
+          <table>
+            <thead>
+              <th>id</th>
+              <th>title</th>
+              <th>date</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td className='board-id'>id1</td>
+                <td className='board-title'><a href='*'>tasdfale1</a></td>
+                <td className='board-date'>date1</td>
+              </tr>
+              <tr>
+                <td className='board-id'>id1</td>
+                <td className='board-title'><a href='*'>tasdfale1</a></td>
+                <td className='board-date'>date1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <button onClick={()=>{
             fetch("http://172.30.1.48:8080/hello3")
             .then((res)=>res.text())
             .then((result)=>console.log(result));
@@ -26,10 +55,40 @@ function App() {
             })
             .then((result) => console.log(result));
           }}>POST</button>
-        </div>
       </div>
     </div>
   );
+}
+
+function Navbar(){
+  let navigate = useNavigate();
+  return(
+    <div className='header'>
+      <div className='navbar'>
+        <a className='navbar-logo' onClick={()=>{navigate('/')}}>Fire Sea</a>
+        <a className='navbar-item' onClick={()=>{navigate('/server')}}>server</a>
+        <a className='navbar-item' onClick={()=>{navigate('/front')}}>front</a>
+      </div>
+    </div>
+  )
+}
+
+function Input(){
+  return(
+    <div className='search'>
+      <div className='search-container'>
+        <input className='search-input' placeholder='검색어를 입력하세요'/>
+        <button className='search-btn'>검색</button>
+      </div>
+    </div>
+  )
+}
+
+function Error(){
+
+  return(
+    <div>err</div>
+  )
 }
 
 export default App;
