577ed562286d9773fd723aeaa3a2f2ad52c3a415
hhss2259 <hhss2259@naver.com>
2023-03-14 14:24:18 +0900

    get, post test

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <div className='navbar'>
+          <h1>Fire Sea</h1>
+          <a>server</a>
+          <a>front</a>
+          <button onClick={()=>{
+            fetch("http://172.30.1.48:8080/hello3")
+            .then((res)=>res.text())
+            .then((result)=>console.log(result));
+          }}>hi</button>
+
+          <button onClick={()=>{
+            fetch("http://172.30.1.48:8080/hello2", {
+              method: "POST",
+              body: JSON.stringify({
+                email: "id",
+                password: "pw"
+              })
+            })
+            .then((result) => console.log(result));
+          }}>POST</button>
+        </div>
+      </div>
     </div>
   );
 }
