da511114dde6c7e29e4ca6ed71ae5cb45e56ae7b
sehyun <germs1020@gmail.com>
2023-03-15 02:50:44 +0900

    메인 이미지, server 및 front 게시판 추가 + 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
 import './App.css';
 import { useParams } from 'react-router-dom';
 import { Routes, Route, useNavigate, Outlet} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function App() {
-
   return (
     <div className="App">
 
       <Navbar/>
 
       <Routes>
-        <Route path="/" element={<Input/>}/>
+        <Route path="/" element={<Main/>}/>
+
+        <Route path="/server" element={<Board category={'server'}/>}/>
+        <Route path="/front" element={<Board category={'front'}/>}/>
+        <Route path="/edit" element={<Edit/>}/>
         <Route path="*" element={<Error/>}/>
       </Routes>
 
       <div className='content'>
 
-        <div className='board'>
-          <h1 className='board-category'>server</h1>
-          <table>
-            <thead>
-              <th>id</th>
-              <th>title</th>
-              <th>date</th>
-            </thead>
-            <tbody>
-              <tr>
-                <td className='board-id'>id1</td>
-                <td className='board-title'><a href='*'>tasdfale1</a></td>
-                <td className='board-date'>date1</td>
-              </tr>
-              <tr>
-                <td className='board-id'>id1</td>
-                <td className='board-title'><a href='*'>tasdfale1</a></td>
-                <td className='board-date'>date1</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        
 
         <button onClick={()=>{
             fetch("http://172.30.1.48:8080/hello3")
@@ -66,14 +49,15 @@ function Navbar(){
     <div className='header'>
       <div className='navbar'>
         <a className='navbar-logo' onClick={()=>{navigate('/')}}>Fire Sea</a>
-        <a className='navbar-item' onClick={()=>{navigate('/server')}}>server</a>
-        <a className='navbar-item' onClick={()=>{navigate('/front')}}>front</a>
+        <a className='navbar-item' onClick={()=>{navigate('/server')}}>Server</a>
+        <a className='navbar-item' onClick={()=>{navigate('/front')}}>Front</a>
       </div>
     </div>
   )
 }
 
 function Input(){
+
   return(
     <div className='search'>
       <div className='search-container'>
@@ -84,6 +68,17 @@ function Input(){
   )
 }
 
+function Main(){
+  return(
+    <>
+      <Input/>
+      <div className='main'>
+        <img src={process.env.PUBLIC_URL + '/main_img.jpg'} width="40%"/>
+      </div>
+    </>
+  )
+}
+
 function Error(){
 
   return(
@@ -91,4 +86,54 @@ function Error(){
   )
 }
 
+function Board({category}){
+  let navigate = useNavigate();
+  let id=9;
+  return(
+    <>
+    <Input/>
+    <div className='board'>
+      <h1 className='board-category'>{category}</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className='board-id'>id1</td>
+            <td className='board-title'><a onClick={()=>{navigate(`${id}`)}}>tasdfsssssale1</a></td>
+            <td className='board-date'>date1</td>
+          </tr>
+          <tr>
+            <td className='board-id'>id1</td>
+            <td className='board-title'><a href='*'>tasdfale1</a></td>
+            <td className='board-date'>date1</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button className='board-new' onClick={()=>{navigate('/edit')}}>글작성</button>
+      <Outlet/>
+    </div>
+    </>
+  )
+}
+
+
+function Edit(){
+  console.log('졸려')
+  return(
+    <>
+    <div>cival</div>
+    <div>cival</div>
+    <div>cival</div>
+    <div>cival</div>
+    <div>cival</div>
+    <div>cival</div></>
+  )
+}
 export default App;
