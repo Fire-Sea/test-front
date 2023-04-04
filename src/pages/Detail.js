92e79c11a1040d14498380a43313f3be64a62bf2
sehyun <germs1020@gmail.com>
2023-04-04 11:34:42 +0900

    글 삭제, 수정 버튼 및 ajax 요청 포맷 추가

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,20 +2,24 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import {useSelector} from 'react-redux';
 import axios from 'axios';
+import {useCookies} from 'react-cookie';
 import { Input } from '../Input';
 import '../styles/Detail.css';
 
 function Detail(){
   const navigate = useNavigate();
+  const [cookies] = useCookies();
   const ip = useSelector((state)=>{return state.ip});
   const [fade, setFade] = useState('');
   const {category, id} = useParams();
   const [textData, setTextData] = useState({
     category: '',
     textTitle: '',
-    textBody: ''
+    textBody: '',
+    nickname: ''
   });
-  
+  const nickname = cookies.nickname;
+
   // 글 정보 저장 함수
   const getData = async ()=>{
     try{
@@ -30,6 +34,49 @@ function Detail(){
     }
   };
 
+  // 글 삭제 요청 함수
+  const deleteData = async ()=>{
+    try{
+      const response = await axios.get(`http://${ip}/api/detail/?category=${category}&delete`);
+      const statusCode = response.data.statusCode;
+      if(statusCode === 20000){
+        alert('성공적으로 삭제되었습니다.')
+      }
+      else{
+        alert('알수없는 이유로 실패했습니다.')
+      }
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+
+  // 글 수정 요청 함수
+  const modifyData = async ()=>{
+    try{
+      const response = await axios.get(`http://${ip}/api/detail/?category=${category}&modify`);
+      const statusCode = response.data.statusCode;
+      if(statusCode === 20000){
+        navigate(`/detail/${category}/${id}`);
+      }
+      else{
+        alert('알수없는 이유로 실패했습니다.')
+      }
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+
+  // 글 수정, 삭제 버튼 출력
+  const showAdminBtn = ()=>{
+    return(
+      <>
+      <button className='detail-modifyBtn'>글 수정하기</button>
+      <button className='detail-removeBtn'>글 삭제하기</button>
+      </>
+    )
+  }
   useEffect(()=>{
     getData();
     const fadeTimer = setTimeout(()=>{setFade('end')}, 100)
@@ -51,6 +98,9 @@ function Detail(){
         <div className='detail-body' dangerouslySetInnerHTML={{__html: textData.textBody}}>
         </div>
         <button className='detail-backBtn' onClick={()=>navigate(-1)}>뒤로가기</button>
+        {
+          nickname == textData.nickname && showAdminBtn()
+        }
     </div>
     </>
     )
