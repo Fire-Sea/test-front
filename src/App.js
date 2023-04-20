e1490e42dbbf1852c6693eb3b5cd243c53f224f3
sehyun <germs1020@gmail.com>
2023-04-21 03:12:33 +0900

    feat: 메인페이지를 대신할 hello 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import {Mypage} from './pages/Mypage';
 import {Main} from './pages/Main';
 import {Footer} from './components/Footer'
 import GlobalStyles from './components/GlobalStyles';
+import { useEffect, useState, useRef } from 'react';
 
 function App() {
   const [themeMode, toggleTheme] = useTheme();
@@ -53,6 +54,7 @@ function App() {
               <Route path="/mypage/:nickname/:currentPage" element={<Mypage/>}/>
               <Route path="/gacha" element={<Gacha/>}/>
               <Route path="/register" element={<Register/>}/>
+              <Route path="/hello" element={<Hello/>}/>
               <Route path="*" element={<Error/>}/>
             </Routes>
             <a href='#pageTop' className='top-btn'><p>Top</p></a>
@@ -63,6 +65,51 @@ function App() {
         </div>
     </ThemeProvider></>
   );
+}
+
+function Hello(){
+  const [scrollY, setScrollY] = useState(window.scrollY);
+  const testRef = useRef([]);
+  useEffect(()=>{
+    window.addEventListener('scroll', handleScroll, {capture: true})
+    return()=>{
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+  const handleScroll = (e)=>{
+    console.log(window.scrollY)
+    const scrollY = window.scrollY;
+    let i = 0;
+    testRef.current.map((el)=>{
+      el.style.transform = `scale(${(97+i)/90 - scrollY/4500})`;
+      el.style.opacity = (16+i)/9 - scrollY/450;
+      i += 10
+    })
+  }
+
+  return(
+  <>
+  <h1 className='hello-h1'>어서오세요!</h1>
+    <div className='hello-container'>
+      <div className='hello-box' ref={el => testRef.current[0] = el}>
+        <img className='hello-img-l' alt='main_img1' src={process.env.PUBLIC_URL + '/main_img1.jpg'}/>
+        <h4>hello-box1</h4>
+      </div>
+      <div className='hello-box' ref={el => testRef.current[1] = el}>
+        <img className='hello-img-r' alt='main_img2' src={process.env.PUBLIC_URL + '/main_img2.jpg'}/>
+        <h4>hello-box2</h4>
+      </div>
+      <div className='hello-box' ref={el => testRef.current[2] = el}>
+        <img className='hello-img-l' alt='main_img1' src={process.env.PUBLIC_URL + '/main_img1.jpg'}/>
+        <h4>hello-box3</h4>
+      </div>
+      <div className='hello-box'>
+        <img className='hello-img-r' alt='main_img2' src={process.env.PUBLIC_URL + '/main_img2.jpg'}/>
+        <h4>hello-box4</h4>
+      </div>
+    </div>
+  </>
+  )
 }
 
 function Error(){
