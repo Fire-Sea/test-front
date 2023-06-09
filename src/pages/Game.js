b8d3a9f7f8ae41af751273c91b14a315e0d6b774
sehyun <germs1020@gmail.com>
2023-06-09 23:39:28 +0900

    feat: 업, 다운 게임 로직 일부 추가

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -3,17 +3,34 @@ import { useState } from 'react';
 
 function Game(){
   let [answer, setAnswer] = useState('');
-
+  let [guess, setGuess] = useState('');
+  let [chance, setChance] = useState(5);
   let onChange = (e)=>{
-    setAnswer(e.target.value);
+    setGuess(e.target.value);
   }
   let onClick = (e)=>{
-    if(answer){
-      alert(answer)
+    if(guess){
+      if(guess > answer){
+        alert('DOWN!')
+        setChance(chance-1)
+      }
+      else if(guess < answer){
+        alert('UP!')
+        setChance(chance-1)
+      }
+      else{
+        alert('정답!')
+      }
     }
     else{
       alert('정답을 입력해주세요!')
     }
+  }
+  let createNum = (e)=>{
+    const answer = Math.floor(Math.random()*101);
+    setAnswer(answer);
+    alert(answer)
+    e.target.style.display = 'none';
   }
   return(
     <>
@@ -22,9 +39,9 @@ function Game(){
         <p>Up & Down 게임은 컴퓨터가 정한 수를 플레이어가 맞추는 게임입니다.</p>
         <p>플레이어는 특정 수를 외치고, 컴퓨터는 이 수가 정답보다 작으면 'Down', 크면 'Up'을 외칩니다.</p>
         <p>숫자는 1부터 100까지이며 정답을 외칠 수 있는 기회는 5번입니다.</p>
-
+        <button onClick={(e)=>{createNum(e); }}>시작</button>
+        <p>현재 남은 횟수: {chance}번</p>
         <div><input onChange={onChange}></input></div>
-
         <button onClick={onClick}>정답 제출하기</button>
       </div>
     </>
